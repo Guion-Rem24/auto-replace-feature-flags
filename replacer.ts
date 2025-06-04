df7f8b936b5eaedb333b636dcf28436d8f5e7b62
Guion-Rem24 <technee.rem24@gmail.com>
2025-06-04 13:34:28 +0900

    ittann soreppokunatta

--- replacer.ts
+++ replacer.ts
@@ -6,7 +6,9 @@ import {
   VariableDeclaration,
   Expression,
   SourceFile,
-} from "ts-morph";
+  ts,
+} from 'ts-morph';
+import { Identifier, Statement, textChangeRangeIsUnchanged } from 'typescript';
 
 type ValueMap = Record<string, boolean | string>;
 
@@ -17,23 +19,40 @@ export class BranchReplacer {
   ) {}
 
   replaceUseValueBranchesWithKey(key: string): void {
-    const declarations = this.project.getSourceFiles().flatMap(file => this.findUseValueDeclarations(key, file));
+    const declarations = this.project
+      .getSourceFiles()
+      .flatMap((file) => this.findUseValueDeclarations(key, file));
 
     for (const decl of declarations) {
       const varName = decl.getName();
       const expected = this.valueMap[key];
 
       const refs = decl.findReferences();
-      const refNodes = refs.flatMap(ref =>
-        ref.getReferences().map(r => r.getNode())
-      );
+      const refNodes = refs
+        .flatMap((ref) => ref.getReferences())
+        .map((r) => r.getNode())
+        .filter((node) => node.getParent() !== decl);
 
       for (const refNode of refNodes) {
-        const parent = refNode.getParent();
+        const { condition: evaluated, parent } = this.resolveCondition(
+          refNode,
+          varName,
+          expected
+        );
+        if (evaluated === undefined) {
+          console.warn(
+            `Could not resolve condition for variable "${varName}" in file "${decl.getSourceFile().getBaseName()}".`
+          );
+          continue;
+        }
+
+        if (!parent) {
+          continue;
+        }
 
         if (Node.isIfStatement(parent)) {
-          const cond = parent.getExpression();
-          const evaluated = this.tryEvaluateCondition(cond, varName, expected);
+          // const cond = parent.getExpression();
+          // const evaluated = this.tryEvaluateCondition(cond, varName, expected);
           if (evaluated === true) {
             parent.replaceWithText(parent.getThenStatement().getText());
           } else if (evaluated === false) {
@@ -45,12 +64,35 @@ export class BranchReplacer {
             }
           }
         } else if (Node.isConditionalExpression(parent)) {
-          const cond = parent.getCondition();
-          const evaluated = this.tryEvaluateCondition(cond, varName, expected);
+          // const cond = parent.getCondition();
+          // const evaluated = this.tryEvaluateCondition(cond, varName, expected);
           if (evaluated === true) {
             parent.replaceWithText(parent.getWhenTrue().getText());
           } else if (evaluated === false) {
             parent.replaceWithText(parent.getWhenFalse().getText());
+          }
+        } else if (Node.isBinaryExpression(parent)) {
+          const op = parent.getOperatorToken().getKind();
+          const [left, right] = [parent.getLeft(), parent.getRight()];
+          const leftEval = this.tryEvaluateCondition(left, varName, expected);
+          // const rightEval = this.tryEvaluateCondition(right, varName, expected);
+          switch (op) {
+            case SyntaxKind.AmpersandAmpersandToken:
+              if (!!leftEval) {
+                parent.replaceWithText(right.getText());
+              } else {
+                parent.replaceWithText('false');
+                // this.trailBracket(parent);
+              }
+              break;
+            case SyntaxKind.BarBarToken:
+              if (!leftEval) {
+                parent.replaceWithText(right.getText());
+              } else {
+                parent.replaceWithText('false');
+                // this.trailBracket(parent);
+              }
+              break;
           }
         }
       }
@@ -64,64 +106,153 @@ export class BranchReplacer {
     }
   }
 
-  findUseValueDeclarations(key: string, file: SourceFile): VariableDeclaration[] {
-    return file.getVariableDeclarations().filter(decl => {
+  trailBracket(node: Node<ts.Node>): void {
+    if (node.wasForgotten()) return;
+    const parent = node.getParent();
+    if (!parent) return;
+    if (Node.isParenthesizedExpression(parent)) {
+      parent.replaceWithText('');
+    }
+  }
+
+  findUseValueDeclarations(
+    key: string,
+    file: SourceFile
+  ): VariableDeclaration[] {
+    return file.getVariableDeclarations().filter((decl) => {
       const initializer = decl.getInitializer();
       if (!initializer || !Node.isCallExpression(initializer)) return false;
 
       const callExpr = initializer;
       const exprName = callExpr.getExpression().getText();
-      if (exprName !== "useValue") return false;
+      if (exprName !== 'useValue') return false;
 
       const args = callExpr.getArguments();
-      return args.length === 1 && args[0].getText().replace(/['"]/g, "") === key;
+      return (
+        args.length === 1 && args[0].getText().replace(/['"]/g, '') === key
+      );
     });
   }
 
+  /**
+   * 再帰的に親を探索して条件を評価
+   */
+  resolveCondition(
+    node: Node<ts.Node>,
+    varName: string,
+    expected: string | boolean
+  ): { condition: boolean | undefined; parent: Node<ts.Node> | undefined } {
+    let parent = node.getParent();
+    if (parent) {
+      const condition = this.tryEvaluateCondition(node, varName, expected);
+      // parent.condition is undefined, the node should be treated as Identifier
+      if (
+        condition !== undefined &&
+        this.tryEvaluateCondition(parent, varName, condition) === undefined
+      ) {
+        return {
+          condition,
+          parent,
+        };
+      }
+    }
+    const tmp = this.tryEvaluateCondition(node, varName, expected);
+    if (tmp === undefined) {
+      return { condition: undefined, parent: node };
+    }
+    let condition: boolean = tmp;
+    while (parent !== undefined) {
+      const parentCondition = this.tryEvaluateCondition(
+        parent,
+        varName,
+        expected
+      );
+      if (parentCondition === undefined) {
+        break;
+      }
+      condition = parentCondition;
+      parent = parent.getParent();
+    }
+
+    return { condition, parent };
+    // if (!parent) {
+    //   return condition;
+    // }
+    // if (
+    //   Node.isBinaryExpression(parent) ||
+    //   Node.isPrefixUnaryExpression(parent) ||
+    //   Node.isIdentifier(parent)
+    // ) {
+    //   const condition = this.tryEvaluateCondition(parent, varName, expected);
+    //   if (typeof condition === 'undefined') {
+    //     return undefined; // 条件が評価できない場合は undefined を返す
+    //   }
+    //   // TODO: condition を渡すで合っているか確認
+    //   const parentCondition = this.resolveCondition(parent, varName, condition);
+
+    //   return parentCondition;
+    // }
+  }
+
+  private getConstValue = (
+    node: Expression
+  ): string | number | boolean | undefined => {
+    if (Node.isStringLiteral(node)) return node.getLiteralText();
+    if (Node.isNumericLiteral(node)) return Number(node.getLiteralText());
+    if (Node.isTrueLiteral(node)) return true;
+    if (Node.isFalseLiteral(node)) return false;
+    return undefined;
+  };
+
   private tryEvaluateCondition(
-    expr: Expression,
+    node: Node<ts.Node>,
     varName: string,
     expected: boolean | string
   ): boolean | undefined {
-    const getConstValue = (node: Expression): string | number | boolean | undefined => {
-      if (Node.isStringLiteral(node)) return node.getLiteralText();
-      if (Node.isNumericLiteral(node)) return Number(node.getLiteralText());
-      if (Node.isTrueLiteral(node)) return true;
-      if (Node.isFalseLiteral(node)) return false;
-      return undefined;
-    };
-
-    if (Node.isBinaryExpression(expr)) {
-      const op = expr.getOperatorToken().getText();
-      const [left, right] = [expr.getLeft(), expr.getRight()];
+    if (Node.isBinaryExpression(node)) {
+      const op = node.getOperatorToken().getKind();
+      const [left, right] = [node.getLeft(), node.getRight()];
 
       const leftText = left.getText();
       const rightText = right.getText();
-      const leftVal = getConstValue(left);
-      const rightVal = getConstValue(right);
+      const leftVal = this.getConstValue(left);
+      const rightVal = this.getConstValue(right);
 
-      if (op === "===" || op === "==") {
-        if (leftText === varName && rightVal !== undefined) return expected === rightVal;
-        if (rightText === varName && leftVal !== undefined) return expected === leftVal;
+      switch (op) {
+        case SyntaxKind.EqualsEqualsToken:
+        case SyntaxKind.EqualsEqualsEqualsToken:
+          if (leftText === varName && rightVal !== undefined)
+            return expected === rightVal;
+          if (rightText === varName && leftVal !== undefined)
+            return expected === leftVal;
+          break;
+        case SyntaxKind.ExclamationEqualsToken:
+        case SyntaxKind.ExclamationEqualsEqualsToken:
+          if (leftText === varName && rightVal !== undefined)
+            return expected !== rightVal;
+          if (rightText === varName && leftVal !== undefined)
+            return expected !== leftVal;
+          break;
+        // case SyntaxKind.AmpersandAmpersandToken:
+        // case SyntaxKind.BarBarToken:
+        //   const leftEval = this.tryEvaluateCondition(left, varName, expected);
+        //   const rightEval = this.tryEvaluateCondition(right, varName, expected);
+        //   if (leftEval === undefined || rightEval === undefined)
+        //     return undefined;
+        //   return op === SyntaxKind.AmpersandAmpersandToken
+        //     ? leftEval && rightEval
+        //     : leftEval || rightEval;
+        default:
       }
-      if (op === "!==" || op === "!=") {
-        if (leftText === varName && rightVal !== undefined) return expected !== rightVal;
-        if (rightText === varName && leftVal !== undefined) return expected !== leftVal;
-      }
-
-      if (op === "&&" || op === "||") {
-        const leftEval = this.tryEvaluateCondition(left, varName, expected);
-        const rightEval = this.tryEvaluateCondition(right, varName, expected);
-        if (leftEval === undefined || rightEval === undefined) return undefined;
-        return op === "&&" ? leftEval && rightEval : leftEval || rightEval;
-      }
-    } else if (Node.isPrefixUnaryExpression(expr)) {
-      const operand = expr.getOperand().getText();
+    } else if (Node.isPrefixUnaryExpression(node)) {
+      // if (!/* <- */ result)
+      const operand = node.getOperand().getText();
       if (operand === varName) {
         return !expected;
       }
-    } else if (Node.isIdentifier(expr)) {
-      if (expr.getText() === varName) return Boolean(expected);
+    } else if (Node.isIdentifier(node)) {
+      // if (result)
+      if (node.getText() === varName) return Boolean(expected);
     }
     return undefined;
   }
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { useValue } from '@some-library'
+import { useValue } from '@some-library';
+
+declare const isAdmin: () => boolean;
+declare const doX: () => void;
+declare const doY: () => void;
 
 const App: React.FC = () => {
   const enabled = useValue('feature-1');
-  return <div>
-    Hello, World!
-  </div>
-}
+  const disabled = useValue('feature-2');
+  if (enabled === 'feature1') {
+    console.log('Feature 1 is enabled');
+  } else if (!!disabled) {
+    console.log('Feature 1 is disabled');
+  } else if (enabled && disabled) {
+    console.log('Feature 1 is not available');
+  }
+  if (isAdmin() || false) doX();
+  else doY();
+  const happy = disabled === 'feature2' ? 'hoge' : 'fuga';
+  const sad = disabled === 'feature2' && enabled;
+  return <div>Hello, World!</div>;
+};
 
-const root = ReactDOM.createRoot(document.querySelector('#root')!)
-root.render(<App/>)
+const root = ReactDOM.createRoot(document.querySelector('#root')!);
+root.render(<App />);
